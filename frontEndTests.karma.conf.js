cca583251f19aed24c77ea11fea083cc3c810ca0
Thomas Quinlan <tquinlan1992@gmail.com>
2017-03-06 18:38:03 -0500

    set up simple test for sampleComponentIndex

--- frontEndTests.karma.conf.js
+++ frontEndTests.karma.conf.js
@@ -2,70 +2,77 @@
 // Generated on Wed Jun 01 2016 17:37:40 GMT-0400 (EDT)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      './build/client/public/app/app.js',
-      './src/client/public/app/components/sample/app.spec.js',
-      './node_modules/angular-mocks/angular-mocks.js'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            './build/client/public/app/app.js',
+            './node_modules/angular-mocks/angular-mocks.js',
+            './src/client/public/app/components/sample/app.spec.js',
+            'build/**/*.html'
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+        // list of files to exclude
+        exclude: [],
 
 
-    // web server port
-    port: 9876,
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'build/**/*.html': ['ng-html2js']
+        },
+
+        ngHtml2JsPreprocessor: {
+            stripPrefix: "build/client/public",
+            moduleName: "app.templates"
+        },
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['spec'],
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // web server port
+        port: 9876,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-});
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    });
 };
--- src/client/public/app/components/sample/app.spec.js
+++ src/client/public/app/components/sample/app.spec.js
@@ -1,6 +1,51 @@
-describe('Users factory', function() {
-  it('has a dummy spec to test 2 + 2', function() {
-    // An intentionally failing test. No code within expect() will never equal 4.
-    expect(2+2).toEqual(4);
-  });
+var describe,
+    beforeEach,
+    expect,
+    it,
+    inject,
+    $compile,
+    $scope,
+    element,
+    angular;
+
+describe('dummy test', function() {
+    it('has a dummy spec to test 2 + 2', function() {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect(2 + 2).toEqual(4);
+    });
+});
+
+describe('Unit testing sample component', function() {
+
+    // Load the myApp module, which contains the directive
+    beforeEach(module("envConfigs", 'sampleComponentModule', 'app.templates'));
+
+    // Store references to $rootScope and $compile
+    // so they are available to all tests in this describe block
+    beforeEach(inject(function($injector, _$compile_, _$rootScope_) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $compile = _$compile_;
+        $scope = _$rootScope_.$new();
+
+    }));
+
+    describe('test controller', function() {
+        it("test values", function() {
+            $scope.objectTest = {
+                text: "objectTest is working"
+            };
+
+            element = angular.element("<sample-component-index test='component working' object-test='objectTest'></sample-component-index>");
+
+            $compile(element)($scope);
+            $scope.$digest();
+            var isolated = element.isolateScope();
+            var $ctrl = isolated.$ctrl;
+            expect($ctrl).toBeDefined();
+            expect($ctrl.test).toEqual("component working");
+            expect($ctrl.objectTest).toEqual({
+                text: "objectTest is working"
+            });
+        });
+    });
 });
