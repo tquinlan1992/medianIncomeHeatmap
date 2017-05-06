23c2777c9236d3b3557962de5f4077a919b28516
Thomas Quinlan <tquinlan1992@gmail.com>
2017-05-06 10:16:15 -0400

    refactor directories

--- src/client/public/app/resourceLanguages/app.js
+++ src/client/public/app/resourceLanguages/app.js
@@ -1,6 +1,8 @@
 const angular = require("angular");
 
-const app = angular.module("getLanguageJSONApp", []);
+const app = angular.module("getLanguageJSONApp", [
+    require("./translate/app")
+]);
 
 app.service("getLanguageJSON", () => {
     "ngInject";
--- src/client/public/app/resourceLanguages/translate/app.js
+++ src/client/public/app/resourceLanguages/translate/app.js
@@ -6,7 +6,6 @@ const app = angular.module("translate-module", [
 
 app.filter("translate", (getLanguageJSON) => {
     "ngInject";
-    console.log("getLanguageJSON", getLanguageJSON);
     function filter(input) {
         return _.get(getLanguageJSON, input);
     }
